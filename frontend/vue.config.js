f6e64f0c12a29bef86e653a983be9b58a83c760c
yuchengH1988 <yucheng.h.1988@gmail.com>
2023-05-11 15:56:28 +0800

    fix frontend host Server setting

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/cafe-inventory-front-vue/'
-    : '/'
+  // publicPath: process.env.NODE_ENV === 'production'
+  //   ? '/my-project/'
+  //   : '/'
+  devServer: {
+    disableHostCheck: true
+  }
 }
